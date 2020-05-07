10db9ff54e892e65e3c8f50c053315c425c70714
MKnoles <mknoles78@gmail.com>
2020-05-06 22:17:43 -0400

    cleaned up files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import Home from "./containers/Home"
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("/api/config")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Home/>
+    </>
+    
+  
   );
 }
 
 export default App;
+
